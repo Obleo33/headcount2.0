203fe354ce791519a725e7781e0ea5adfb64200d
Franklin Crosby <obleo33@gmail.com>
2017-03-15 16:05:24 -0600

    refactor findByName, adjust case insensitivity, add didMount to headCount

--- test/unit/iteration-1-part1.test.js
+++ test/unit/iteration-1-part1.test.js
@@ -29,15 +29,14 @@ describe('DistrictRepository iteration 1 - part 1', () =>  {
     const academy = district.findByName('ACADEmY 20');
 
     expect(academy.location).toEqual('ACADEMY 20');
-    expect(typeof academy.data).toBe('object');
+    expect(typeof academy.yearlyData).toBe('object');
   });
 
   test('district data is rounded to the nearest hundredth', () => {
     const result = {"2004": 0.302, "2005": 0.267, "2006": 0.354, "2007": 0.392, "2008": 0.385, "2009": 0.39, "2010": 0.436, "2011": 0.489, "2012": 0.479, "2013": 0.488, "2014": 0.49}
     const academy = district.findByName('ACADEmY 20');
-
     //added extra data for nesting
-    expect(academy.data.yearlyData).toEqual(result);
+    expect(academy.yearlyData).toEqual(result);
   });
 
   test('district data is sanitized and defaults to 0', () => {
@@ -45,7 +44,7 @@ describe('DistrictRepository iteration 1 - part 1', () =>  {
     const result = {"2004": 1, "2005": 0, "2006": 0.125, "2007": 0, "2008": 1, "2009": 1, "2010": 1, "2011": 1, "2012": 1, "2013": 1, "2014": 1}
 
     //added extra data for nesting
-    expect(academy.data.yearlyData).toEqual(result)
+    expect(academy.yearlyData).toEqual(result)
   });
 
 });
