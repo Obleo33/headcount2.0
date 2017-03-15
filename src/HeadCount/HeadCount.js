786fe943d54a12f917ca4a2dd0aa68c1ea3d9a92
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-03-15 14:30:58 -0600

    seaving search changes. have object console logging but nothing else

--- src/HeadCount/HeadCount.js
+++ src/HeadCount/HeadCount.js
@@ -8,19 +8,32 @@ export default class HeadCount extends Component {
   constructor() {
     super()
     this.state = {
-      data: new DistrictRepository(kinderData)
+      data: new DistrictRepository(kinderData),
     }
   }
+
+  displayMatches(e){
+    const match = this.state.data.findAllMatches(e.target.value)
+    console.log(match);
+  }
+
 
   render() {
     return(
       <div className="head-count">
         <h1>HeadCount2.0</h1>
+        <form className="search-form">
+          <input className="search-input"
+                 placeholder="search"
+                 onChange={this.displayMatches.bind(this)}
+                 />
+        </form>
         <DistrictCards data={this.state.data}/>
       </div>
     )
   }
 }
+
 
 HeadCount.propType = {
   data: React.PropTypes.object.isRequired
