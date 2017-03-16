db540015830790b457b67709858fb0f532542a0d
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-03-16 08:29:59 -0600

    css layout worked on as well as some work on onClick for district cards

--- src/DistrictCards/DistrictCards.js
+++ src/DistrictCards/DistrictCards.js
@@ -3,11 +3,17 @@ import DistrictYears from './DistrictYears';
 import './DistrictCards.css';
 
 const DistrictCards = (data) => {
+
+  function handleClick() {
+    // this.toggleClass('clicked')
+    return console.log('this was clicked');
+  }
+
   return (
     <div className="district-container">
       {data.data.map((value, index) => {
         return (
-          <div className="district-card" key={index}>
+          <div className="district-card" key={index} onClick={() => handleClick()}>
             <h2 className="district-name">{value.location}</h2>
             <DistrictYears yearlyData={value.yearlyData}/>
           </div>
